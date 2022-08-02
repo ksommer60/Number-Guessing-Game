f8fd07c8fb7f10f04b508eaa8d72195dc1b8caa0
ksommer60 <105603761+ksommer60@users.noreply.github.com>
2022-08-02 13:14:37 -0400

    Add files via upload

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! Wait for the prompt to enter in a number!");
+            NumberGenerator();
+        }
+
+        static void NumberGenerator()
+        {
+            Random randomNumber = new Random();
+            int winningNumber = randomNumber.Next(0, 100);
+            bool win = false;
+
+            do
+            {
+                Console.WriteLine("Guess a number inbetween 0 and 100");
+                string userInput = Console.ReadLine();
+
+                int number = int.Parse(userInput);
+                int numberDifference = winningNumber - number; //display what the difference in the numbers is 
+
+                if (number > winningNumber)
+                {
+                    Console.WriteLine("Your guess was off by " + numberDifference);
+                }
+                else if (number < winningNumber)
+                {
+                    Console.WriteLine("Your guess was off by " + numberDifference);
+                }
+                else
+                {
+                    Console.WriteLine("You win!!");
+                    win = true;
+                }
+
+            } while (win == false);
+        }
+    }
+}
